dffe1252850fe0f4e96ccab599386bebb0d3b05c
Benni <benni@geekmail.de>
2026-06-27 16:22:33 +0200

    Create core Skeleton

---
+++ src/config/config.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#########################################################
+# FRITZ!View Configuration
+#########################################################
+
+INSTALL_MODE="CLIENT"
+
+MASTER_IP="192.168.178.1"
+
+MASTER_PORT="49443"
+
+MASTER_USER=""
+
+MASTER_PASS=""
+
+DISPLAY_WIDTH=320
+
+DISPLAY_HEIGHT=240
+
+UPDATE_INTERVAL=5
+
+THEME="AVM"
+
+LOGLEVEL=1
